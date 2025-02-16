3c29942e1c00c06096fb0fc3e97bd395fe4deda6
shifa-khan <shifa7304@gmail.com>
2025-02-16 00:56:22 -0500

    Added requirments.txt

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,22 +2,4 @@ from django import forms
 from .models import Assignment, ClassNote, Syllabus
 
 # Form for Assignments
-class AssignmentForm(forms.ModelForm):
-    class Meta:
-        model = Assignment
-        fields = ['title', 'description', 'file', 'due_date']
-        widgets = {
-            'due_date': forms.DateInput(attrs={'type': 'date'}),
-        }
 
-# Form for Class Notes
-class ClassNoteForm(forms.ModelForm):
-    class Meta:
-        model = ClassNote
-        fields = ['title', 'description', 'file']
-
-# Form for Syllabus
-class SyllabusForm(forms.ModelForm):
-    class Meta:
-        model = Syllabus
-        fields = ['course_name', 'file']
